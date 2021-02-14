8e549c07cc213eaad48243007ba4bcf19353f66c
Gjc452 <1796444035@qq.com>
2021-02-14 23:33:53 +0800

    添加 'NotFound' 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    当前页面不存在
+    <router-link to="/money">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
