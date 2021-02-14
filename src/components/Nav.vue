c81fb7322a94ff1cfb76a7f96592416abccc3cf5
Gjc452 <1796444035@qq.com>
2021-02-15 01:37:31 +0800

    解决 svg 的引入问题，并创建<Icon/>组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <router-link to="/Labels">标签</router-link>
-    <router-link to="/Statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+
+    <router-link to="/Labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
+
+    <router-link to="/Statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Nav'
+  name: 'Nav',
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-  }
+.nav {
+}
 </style>
