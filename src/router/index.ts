6d1a4f75fe692dbeb3766da6742f4aa2e39baf86
Gjc452 <1796444035@qq.com>
2021-02-18 00:17:53 +0800

    添加 EditLabel 页面及 Labels 页面标签可跳转至 EditLabel 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,40 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
 import EditLabel from '@/views/EditLabel.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path:'/',
-    redirect:'/money'
+    path: '/',
+    redirect: '/money'
   },
   {
-    path:'/money',
-    component:Money
-  },{
-    path:'/labels',
-    component:Labels
-  },{
-    path:'/statistics',
-    component:Statistics
+    path: '/money',
+    component: Money
+  }, {
+    path: '/labels',
+    component: Labels
+  }, {
+    path: '/statistics',
+    component: Statistics
   },
   {
-    path:'/labels/edit',
-    component:EditLabel
+    path: '/labels/edit/:id',
+    component: EditLabel
   },
   {
-    path:'*',
-    component:NotFound
+    path: '*',
+    component: NotFound
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
